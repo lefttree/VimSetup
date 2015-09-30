c6854e277afea35c5278a4f3a57a114b6e0235de
Xiang <xiang@silvustechnologies.com>
2015-09-30 16:03:14 -0700

    start writing install script

---
+++ install.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# refer  spf13-vim bootstrap.sh`
+BASEDIR=$(dirname $0)
+cd $BASEDIR
+CURRENT_DIR=`pwd`
+
+lnif() {
+    if [ -e "$1" ]; then
+        ln -sf "$1" "$2"
+    fi
+}
+
+echo "Step1: backing up current vim config"
+today=`date +%Y%m%d`
+for i in $HOME/.vim $HOME/.vimrc $HOME/.gvimrc $HOME/.vimrc.bundles; do [ -e $i ] && [ ! -L $i ] && mv $i $i.$today; done
+for i in $HOME/.vim $HOME/.vimrc $HOME/.gvimrc $HOME/.vimrc.bundles; do [ -L $i ] && unlink $i ; done
+
+echo "Step2: setting up symlinks"
+lnif $CURRENT_DIR/vimrc $HOME/.vimrc
+lnif $CURRENT_DIR/vimrc.bundles $HOME/.vimrc.bundles
+lnif "$CURRENT_DIR/" "$HOME/.vim"
+
+
+echo "Step3: install pathogen"
+
+cd $HOME/.vim/bundle
+git clone https://github.com/tpope/vim-pathogen.git
+
+echo "Step4: clone plugins from github"
+
+#read a list file?
+
+echo "Step5: compile YouCompleteMe"
+
+
+echo "Install Done"
