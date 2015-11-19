a653b3944075d13fca4a18b7a14c576b7d113f83
Xiang <xiang@silvustechnologies.com>
2015-11-18 18:47:51 -0800

    install script:     - link .vimrc     - install pathegon

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# refer  spf13-vim bootstrap.sh`
 BASEDIR=$(dirname $0)
 cd $BASEDIR
 CURRENT_DIR=`pwd`
@@ -18,14 +17,16 @@ for i in $HOME/.vim $HOME/.vimrc $HOME/.gvimrc $HOME/.vimrc.bundles; do [ -L $i 
 
 echo "Step2: setting up symlinks"
 lnif $CURRENT_DIR/vimrc $HOME/.vimrc
-lnif $CURRENT_DIR/vimrc.bundles $HOME/.vimrc.bundles
+#lnif $CURRENT_DIR/vimrc.bundles $HOME/.vimrc.bundles
 lnif "$CURRENT_DIR/" "$HOME/.vim"
 
 
 echo "Step3: install pathogen"
 
-cd $HOME/.vim/bundle
-git clone https://github.com/tpope/vim-pathogen.git
+#cd $HOME/.vim/bundle
+#git clone https://github.com/tpope/vim-pathogen.git
+mkdir -p $HOME/.vim/autoload $HOME/.vim/bundle && \
+curl -LSso $HOME/.vim/bundle/pathogen/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 echo "Step4: clone plugins from github"
 
