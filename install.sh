a36a1ffac07bee7cef9cff477a89f7f5799d7848
lefttree <bylixiang@gmail.com>
2015-11-18 19:30:17 -0800

    Feat: clone plugins

--- install.sh
+++ install.sh
@@ -12,6 +12,7 @@ lnif() {
 
 echo "------------------------------------"
 echo "Step1: backing up current vim config"
+echo "------------------------------------"
 
 today=`date +%Y%m%d`
 for i in $HOME/.vim $HOME/.vimrc $HOME/.gvimrc $HOME/.vimrc.bundles; do [ -e $i ] && [ ! -L $i ] && mv $i $i.$today; done
@@ -19,30 +20,45 @@ for i in $HOME/.vim $HOME/.vimrc $HOME/.gvimrc $HOME/.vimrc.bundles; do [ -L $i 
 
 echo "------------------------------------"
 echo "Step2: setting up symlinks"
+echo "------------------------------------"
 lnif $CURRENT_DIR/vimrc $HOME/.vimrc
 #lnif $CURRENT_DIR/vimrc.bundles $HOME/.vimrc.bundles
-lnif "$CURRENT_DIR/" "$HOME/.vim"
+#lnif "$CURRENT_DIR/" "$HOME/.vim"
+cp -r $CURRENT_DIR/colors $HOME/.vim/colors
 
 
 echo "------------------------------------"
 echo "Step3: install pathogen"
+echo "------------------------------------"
 
 #cd $HOME/.vim/bundle
 #git clone https://github.com/tpope/vim-pathogen.git
-mkdir -p $HOME/.vim/autoload $HOME/.vim/bundle && \
-curl -LSso $HOME/.vim/bundle/pathogen/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+mkdir -p $HOME/.vim/bundle
+cd $HOME/.vim/bundle
+git clone https://github.com/tpope/vim-pathogen.git
+cd -
 
 echo "------------------------------------"
 echo "Step4: clone plugins from github"
+echo "------------------------------------"
 
-cd $HOME/.vim/bundle
 while read -r line
 do
     link=`echo $line | cut -d" " -f2`
-    git clone $link
+    #dirName=`echo $link | sed -n 's/^.*\/\(.*\).git$/\1/g'`
+    cd $HOME/.vim/bundle && git clone $link
 done < "pluginList"
+cd -
 
 echo "Step5: compile YouCompleteMe"
 
+#
+# youcompleteme
+# DoxygenTooklit
+# pyflake
+# tagbar
+# tag list
+# ultisnips
+#
 
 echo "Install Done"
